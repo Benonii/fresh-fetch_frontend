4d3e215b8a7c76b4f803b5a29af8e94b0cecf374
BENONI ESCKINDER <you@example.com>
2024-06-22 17:01:19 +0300

    Removed header link to home page

--- fresh-fetch/src/components/Header.jsx
+++ fresh-fetch/src/components/Header.jsx
@@ -1,15 +1,15 @@
 import "../styles/Header.css";
-import { useNavigate } from "react-router";
+// import { useNavigate } from "react-router";
 
 export default function Header() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const goToProducePage = () => {
-        navigate('/produce');
-    }
+    // const goToProducePage = () => {
+    //     navigate('/produce');
+    // }
     return (
         <div className="header">
-            <h1 onClick={goToProducePage}>Fresh Fetch</h1>
+            <h1>Fresh Fetch</h1>
         </div>
     );
 }
